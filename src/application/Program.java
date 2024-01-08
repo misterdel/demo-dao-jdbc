b1731faa9c2e0ed51e15c27188963fe50bd62c89
André Cahango <misterdel04@hotmail.com>
2024-01-08 18:16:52 +0000

    seller upd entities class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departmant;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departmant dp = new Departmant(1,"Teste");
 		System.out.println(dp.toString());
+		
+		Seller s = new Seller(21, "bob", "bob@gmail.com", new Date(), 1000.0, dp);
+		
+		System.out.println(s.toString());
+
 	}
 
 }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,5 +1,137 @@
 package model.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	private Departmant departmant;
+
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Departmant departmant) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.departmant = departmant;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the birthDate
+	 */
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	/**
+	 * @param birthDate the birthDate to set
+	 */
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	/**
+	 * @return the baseSalary
+	 */
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	/**
+	 * @param baseSalary the baseSalary to set
+	 */
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	/**
+	 * @return the departmant
+	 */
+	public Departmant getDepartmant() {
+		return departmant;
+	}
+
+	/**
+	 * @param departmant the departmant to set
+	 */
+	public void setDepartmant(Departmant departmant) {
+		this.departmant = departmant;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", departmant=" + departmant + "]";
+	}
 
 }
