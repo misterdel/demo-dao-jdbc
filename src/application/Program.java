29e64dcff00fde49cfe99a45e88cc501474a9e67
André Cahango <misterdel04@hotmail.com>
2024-01-08 18:33:02 +0000

    Dao factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,20 @@
 package application;
 
-import java.util.Date;
-
-import model.entities.Departmant;
-import model.entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Departmant dp = new Departmant(1,"Teste");
-		System.out.println(dp.toString());
+		/**Departmant dp = new Departmant(1,"Teste");
+		System.out.println(dp.toString());	
 		
 		Seller s = new Seller(21, "bob", "bob@gmail.com", new Date(), 1000.0, dp);
 		
 		System.out.println(s.toString());
-
+*/
+		
+		SellerDao sellerDao = DaoFactory.creatSellerDao();
 	}
 
 }
