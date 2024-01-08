b6f27a595650198b58b7e3392688b57ba5215200
André Cahango <misterdel04@hotmail.com>
2024-01-08 18:08:58 +0000

    departmant entities class

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Departmant;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departmant dp = new Departmant(1,"Teste");
+		System.out.println(dp.toString());
+	}
+
+}
---
+++ src/model/entities/Departmant.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Departmant implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private int id;
+	private String name;
+
+	public Departmant() {
+	}
+
+	public Departmant(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departmant other = (Departmant) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Departmant [id=" + id + ", name=" + name + "]";
+	}
+
+}
